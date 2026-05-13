161eed3a4c5ec96be9a9dc611032099f3cbbbac5
aoife <96482169+aoleee@users.noreply.github.com>
2026-05-13 14:21:20 +0100

    created components

---
+++ src/app/my-team/my-team.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-team',
+  imports: [],
+  templateUrl: './my-team.html',
+  styleUrl: './my-team.css',
+})
+export class MyTeam {}
