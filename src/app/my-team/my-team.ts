c74646e36b54ead39d6f0fa87ec1182f24252967
aoife <96482169+aoleee@users.noreply.github.com>
2026-05-13 16:52:56 +0100

    had a routerlink error in my-teams.ts

--- src/app/my-team/my-team.ts
+++ src/app/my-team/my-team.ts
@@ -1,12 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { PokemonapiService } from '../services/pokemonapi-service';
 import { CommonModule } from '@angular/common'; 
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-my-team',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './my-team.html',
   styleUrl: './my-team.css'
 })
